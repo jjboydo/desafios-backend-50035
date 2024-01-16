e3893341e783a71cd78dc4a606dca679c48b4c04
Juan Jose Boydo <juanjoo1020@gmail.com>
2024-01-16 12:07:58 -0300

    Servidor en express

---
+++ desafio-3/src/app.js
@@ -0,0 +1,31 @@
+import express, { json } from "express"
+import ProductManager from './ProductManager.js'
+
+const PORT = 8080
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(json())
+
+const productManager = new ProductManager()
+
+app.get('/products', async (req, res) => {
+    const limit = parseInt(req.query.limit)
+    const products = await productManager.getProducts()
+    const getProducts = (!isNaN(limit) && limit > 0) ? products.slice(0, limit) : products
+    res.json(getProducts)
+})
+
+app.get('/products/:pid', async (req, res) => {
+    const productId = parseInt(req.params.pid)
+
+    const products = await productManager.getProducts()
+    const product = products.find(prod => prod.id === productId)
+
+    if (!product) {
+        res.status(404).json({ error: `Product ${productId} does not exist!` })
+    } else {
+        res.json(product)
+    }
+})
+
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`))
